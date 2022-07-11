edf0e22db9cbaf7538a2d5b573c1ea0fe3128ca7
minhaz74692 <minhaz74692@gmail.com>
2022-07-11 18:47:26 +0600

    EJS Blog Website initial commit

---
+++ app.js
@@ -0,0 +1,68 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash")
+
+// const uuidv4 =require("uuid");
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const posts = [];
+//Router:1 Home
+app.get("/", (req, res) => {
+  res.render("home", { homeContent: homeStartingContent, posts: posts, _: _ });
+});
+
+//Router:2 About
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent: aboutContent })
+});
+
+//Router:3 Contact
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent: contactContent })
+});
+
+//Router:4 Compose
+app.get("/compose", (req, res) => {
+  res.render("compose", {})
+});
+app.get("/post/:id", (req, res) => {
+  const postId = req.params.id
+  let thisPost = {}
+  posts.forEach((e) => {
+    if (_.lowerCase(e.title) === _.lowerCase(postId)) {
+      res.render('post', { thisItem: e })
+    }
+  });
+});
+app.post("/compose", (req, res) => {
+  const post = { title: req.body.title, content: req.body.textarea, }
+  posts.push(post);
+  // console.log(posts);
+  res.redirect("/");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
